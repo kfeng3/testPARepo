ac7a3032c5efacc4858fda6ee035a30642913e64
Kai Lai Feng <kfeng3@computerart.net>
2020-01-28 14:55:36 -0500

    Made a ruleBasedPlacer

---
+++ Assets/script/ruleBasedGeometryPlacer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ruleBasedGeometryPlacer : MonoBehaviour
+{
+    public GameObject thing;
+
+    
+
+    public Vector3 position;
+    public Vector3 scale;
+    public Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 jiggle = new Vector3(position.x, position.y + Random.value, position.z);
+        this.transform.position = position;
+        this.transform.localScale = scale;
+        this.transform.localEulerAngles = rotation;
+    }
+}
